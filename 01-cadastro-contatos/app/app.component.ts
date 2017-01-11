ca22501a9a11019bbf974ac4a711587f4756e43f
Ramon Francisco Torres Bezerra <rfbezerra@stefanini.com>
2017-01-11 15:31:21 -0200

    Projeto Startado

---
+++ 01-cadastro-contatos/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component(
+    {
+        selector: 'my-app',
+        template: '<h1> My First Angular 2 App - Edit </h1>'
+    }
+)
+export class AppComponent {
+    
+}
